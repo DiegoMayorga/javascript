03274cca895cc5edc93b24578d9be65753600091
devdiegomt <devdiegomt@gmail.com>
2025-05-28 08:04:45 -0500

    feat: introduction done.

---
+++ 01-introduction.js
@@ -0,0 +1,23 @@
+/* JavaScript */
+
+// Es un lenguaje de programación debilmente tipado que se compila en tiempo de ejecución.
+// Puede ser ejecutado como parte de una página web en un navegador o directamente en
+// cualquier máquina (entorno anfitrión)
+
+// JavaScript fue creado para hacer páginas web más dinámicas (ej. cambiar contenido en una página directamente desde dentro del navegador).
+// Originalmente, fue llamado LiveScript pero dado la popularidad de Java, fue renombrado a JavaScript.
+
+// JavaScript es totalmente independiente de Java. No tiene nada en común.
+
+/* Cómo funcionan las páginas web */
+
+// Usuario --Visita e interactua con--> Cliente (navegador) --Este hace requests a--> Servidor --devuelve responses a--> Cliente (navegador).
+
+// Ejemplo: Un usuario puede clickear un botón y enviar el contenido de un formulario, el navegador hace un request, el servidor maneja información con
+// una base de datos y devuelve un response satisfactorio o insatisfactorio.
+
+// JavaScript hace este proceso más reactivo.
+
+/* JavaScript en acción */
+
+// Hay un floder (01-javascript-in-action) en el que veré un ejemplo de lo antes mencionado.
