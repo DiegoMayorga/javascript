468996cfbe9fdd17299d78d723e3e8433bc798fc
devdiegomt <devdiegomt@gmail.com>
2025-05-28 09:49:58 -0500

    feat: course content updated.

--- 03-course-content.js
+++ 03-course-content.js
@@ -18,3 +18,28 @@
 // 3. Practicar (hay proyectos y quizes).
 // 4. Debuggear y buscar al encontrar vacíos o errores. Usar el código adjunto, Google, la búsqueda de Udemy.
 // 5. Preguntar y responder.
+
+/*
+Using Course Resources
+
+This is a video-on-demand course, so information is of course conveyed via videos.
+But this does not mean that you only get videos!
+Instead, you find plenty of useful additional course resources which you absolutely should utilize as well!
+To be precise, you'll find these kind of resources in the course:
+
+Course Source Code:
+All code that we write is attached to multiple lectures per module (= a snapshot of the code we wrote up to that lecture) and always attached to the last lecture
+of each module (all snapshots for that module and the final code).
+
+Extra Information / Text Lectures:
+The vast majority of the course lectures are video lectures - but occasionally, you'll find a text lecture with some extra information here and there.
+These extra lectures (which are part of the regular course flow) provide some extra information that is "nice to have" or which is simply easier to refer to in written form.
+
+Course Slides:
+The focus of this course is on writing code but some theoretical foundations are best set via slides. All crucial slides are attached to the lectures in which they are shown.
+
+External Links: 
+From time to time I also link to some external resources (I do mention it in the video then, you also will often find some extra, optional links at the end of each module,
+in the last lecture). Going through these resources is of course not required but might be helpful to dive deeper or explore certain niche or highly advanced concepts a bit closer.
+
+*/
