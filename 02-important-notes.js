2f80f7dfdd1c1a73ac1833f91efb9d7efbd8348a
devdiegomt <devdiegomt@gmail.com>
2025-05-28 09:03:37 -0500

    feat: how javascript executes.

---
+++ 02-important-notes.js
@@ -0,0 +1,9 @@
+/* How is JavaScript Executed? */
+
+// Mi código tiene un efecto en la página web a través del motor JavaScript, que está incorporado en el navegador.
+// En Chrome, el nombre del motor es V8, en Firefox es SpiderMonkey y cada uno tiene su propio motor.
+
+// Lo importante es que analiza el código, compila a código de máquina y ejecuta el código de la máquina, para así tener efecto en páginas web.
+// Todo esto lo hace en un solo hilo.
+
+/* Single Threading es un concepto fundamental en JavaScript */
