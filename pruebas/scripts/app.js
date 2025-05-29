7554232dfb0b85d83736a4916d1834c525e5c002
devdiegomt <devdiegomt@gmail.com>
2025-05-29 10:55:59 -0500

    some tests added to practice.

---
+++ pruebas/scripts/app.js
@@ -0,0 +1,24 @@
+/* Header */
+const pageHeader = document.querySelector(".header");
+
+/* Content */
+const pageContent = document.querySelector(".content");
+
+/* Footer */
+const footer = document.querySelector("footer");
+
+/* const darkModeBtn = document.createElement("button");
+pageContent.innerHTML = `<p>Hello World!</p>`;
+
+darkModeBtn.textContent = "Dark Mode";
+darkModeBtn.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+  const result = document.body.classList.toggle("toggle");
+  darkModeBtn.textContent = result ? "☼" : "☾";
+  pageContent.classList.toggle("toggle");
+}
+
+document.body.appendChild(darkModeBtn);
+document.body.appendChild(pageContent);
+document.body.appendChild(homePage); */
