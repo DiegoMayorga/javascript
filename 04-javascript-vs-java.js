33eaeefee995b860c2e84bb7ed2feded73137c6f
devdiegomt <devdiegomt@gmail.com>
2025-05-29 11:08:34 -0500

    js vs java

--- 04-javascript-vs-java.js
+++ 04-javascript-vs-java.js
@@ -1,2 +1,33 @@
 /* JavaScript VS Java */
 
+/* Son dos lenguajes de programación totalmente diferentes con
+diferentes sintaxis y principios. */
+
+/* Java no corre en el navegador */
+// Java es fuertemente tipado y orientado a objetos.
+// JavaScript es debilmente tipado, más flexible.
+
+// Se llamó JavaScript para tener mayor alcance y reconocimiento.
+// Java no corre en el navegador, JavaScript sí.
+
+/* Lado del cliente (Navegador) VS Lado del servidor (NodeJS) */
+
+/* Lado del cliente (navegador) */
+// Lo original de JavaScript
+// Diferentes compañías proveen su propio motor de ejecución de JS (ej, V8)
+// Permite la interación con la página web y las APIs del navegador (ej. ubicación del usuario)
+
+/* Lado del servidor (NodeJS) */
+// ¿Por qué no usar JavaScript fuera del navegador? Pemite la reutilización de código y conocimiento.
+// Extrajo el motor V8 para correr JavaScript en cualquier lugar.
+// APIs especiales fuera del navegador (ej, para trabajar con el sustema de archivos, solicitudes HTTP
+// entrantes, etc.)
+
+/* La sintaxis, los conceptos catacterísticas principales, etc, son exactamente los mismos. */
+
+/* ¿Por qué el lado del cliente primero? */
+
+// 1. Es el origen de JavaScript.
+// 2. No hay alternativas para JavaScript en el navegador.
+// 3. NodeJS usa la misma sintaxis --> Fácilmente puedo aprender NodeJS luego de JS en general.
+// 4. Es más divertido aprenderlo con una buena UI.
