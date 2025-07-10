96c88c837e5b57051f74e6302240fc67132b828a
devdiegomt <devdiegomt@gmail.com>
2025-07-10 09:24:10 -0500

    new files added.

---
+++ 05-history.js
@@ -0,0 +1,14 @@
+/* Key data about JS history */
+
+// ECMA: Asociación Manufacturera de Computadores Europea.
+
+// ECMA SCRIPT
+// Una organización de estándares que evoluciona el lenguaje ECMAScript
+// No es directamente usado pero los proveedores de navegación (Google con Chrome, Mozilla con Firefox, etc)
+// implementan los estándares en sus motores JS.
+// Bajo desarrollo activo.
+
+// JavaScript
+// La más famosa implementación de ECMAScript (otros serían "ActionScript" o "Jscript")
+// Cada navegador viene con su propio motor JS que también define qué características son soportadas.
+// Bajo desarrollo activo.
