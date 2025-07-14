b80eba2cd29ac626b31a406f7d7be52c2cb207e8
devdiegomt <devdiegomt@gmail.com>
2025-07-14 17:11:38 -0500

    data types.

--- 07-tiposdedato.js
+++ 07-tiposdedato.js
@@ -1,18 +1,19 @@
 /* Números */
-2, -3, 22.956
+2, -3, 22.956;
 // Importante para cálculos y código donde necesito trabajar con números.
 
 /* Strings (Texto) */
-"Hola", 'Hola', `Hola`, ''
-// Importante para trabajar con texto, palabras, etc.
+"Hola",
+  "Hola",
+  `Hola`,
+  ""// ¿Cómo se ejecuta un script? // Importante para trabajar con texto, palabras, etc.
+  // El código se ejecuta de arriba a abajo, línea por línea.
 
-// ¿Cómo se ejecuta un script?
-// El código se ejecuta de arriba a abajo, línea por línea.
+  /* Más sobre strings */
+  // Puedo usar comillas simples entre comillas doble, comillas dobles entre comillas simples y cualquiera de las
+  // anteriores entre backticks.
+  `'Hola'` // 'Hola' Con los template literals puedo hacer saltos de línea. Si en css pongo white-space:pre; se verán los saltos de línea.
+  `"Hola"`; // "Hola" Con los template literals también puedo insertar contenido dinámico, como variables.
+("'Hola'"); // 'Hola'
+('"Hola"'); // "Hola"
 
-/* Más sobre strings */
-// Puedo usar comillas simples entre comillas doble, comillas dobles entre comillas simples y cualquiera de las
-// anteriores entre backticks.
-`'Hola'` // 'Hola'
-`"Hola"` // "Hola"
-"'Hola'" // 'Hola'
-'"Hola"' // "Hola"
