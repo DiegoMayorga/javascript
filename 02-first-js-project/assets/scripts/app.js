7d60eb1b8faecc03f6431a647402df38b6520187
devdiegomt <devdiegomt@gmail.com>
2025-07-16 18:28:45 -0500

    cleaning code.

--- 02-first-js-project/assets/scripts/app.js
+++ 02-first-js-project/assets/scripts/app.js
@@ -6,11 +6,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // Se puede inicializar una variable con otra variable como valor. Sería una copia de esa constante.
 
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
 function add() {
-  currentResult = currentResult + parseInt(userInput.value); // Cualquier cosa que obtengo de un input es de tipo string. Debo convertirlo a número.
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`; // No importa si lo parseo a entero, igual será string porque está dentro de template literals.
+  currentResult = currentResult + enteredNumber; // Cualquier cosa que obtengo de un input es de tipo string. Debo convertirlo a número.
   // Una forma rápida de convertir un string a número es usar el operador + antes del valor. La otra forma es con Number(), un método que me permite
   // castearlo a número. parseInt() es otra opción. Con parseFloat también lo convierto en un número, pero en este caso es flotante.
-  outputResult(currentResult, "");
+  outputResult(currentResult, calcDescription);
 }
 // Para las funciones, el comportamiento de top to bottom no aplica. Puedo crear funciones después de usarlas.
 // Sin embargo, es más entendible si primero la creo y luego la utilizo.
