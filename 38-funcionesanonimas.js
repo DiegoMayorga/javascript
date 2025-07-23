236535e25dd0058c3c624bf4d3fe944707bc474b
devdiegomt <devdiegomt@gmail.com>
2025-07-23 15:39:33 -0500

    anonymous functions.

--- 38-funcionesanonimas.js
+++ 38-funcionesanonimas.js
@@ -1,1 +1,19 @@
 /* Funciones anónimas */
+
+// Escenario 1
+
+const sum = function (a, b) {
+  return a + b;
+};
+console.log(sum(2, 3));
+
+// Escenario 2
+
+someButton.addEventListener("click", function () {
+  alert("Hola");
+});
+
+// Escenario 3
+setTimeout(function () {
+  alert("Hola");
+}, 3000);
